3498c528525442032f8fb766572c939b899eb9b5
Eduardo <eduardo301997@gmail.com>
2023-03-20 16:08:52 -0500

    Fin seccion 11 -version 4.0.0

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/user.model');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req,res = response) => {
     
@@ -49,6 +50,62 @@ const login = async(req,res = response) => {
 }
 
 
+const googleSignIn = async(req, res= response)=>{
+
+    const { id_token } = req.body;
+
+    try {
+        const {name, img, email } = await googleVerify( id_token );
+        
+
+        let usuario = await Usuario.findOne({email});
+
+        if(!usuario) {
+            //Tengo que crearlo
+            const data = {
+                name,
+                email,
+                password: ':P',
+                img,
+                google: true
+            }
+        };
+
+        usuario = new Usuario(data);
+        await usuario.save();
+
+        // si el usuario en DB
+
+        if(!usuario.status){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+
+        const token = await generarJWT(usuario.id);
+        res.json({
+            usuario,
+            token
+        })
+
+        res.json({
+            msg: 'Todo bien!',
+            id_token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El tojen no se pudo verificar'
+        })
+    }
+
+}
+
+
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
